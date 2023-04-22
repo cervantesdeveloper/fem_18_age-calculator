807334ed023c12fc6d2de07f2e375125e964a5e2
Victor <cervantesdeveloper@gmail.com>
2023-04-22 17:57:23 -0600

    I added a screen shot and a new readme

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import './App.css'
 import FormAge from './components/FormAge'
+import { createPortal } from 'react-dom'
+import Footer from './components/Footer';
+
+const footer = document.getElementById("footer");
 
 function App() {
   return (
-    <div className="App">
-      <FormAge />
-
-    </div>
+    <>
+      <div className="App">
+        <FormAge />
+      </div>
+      {createPortal(
+        <Footer />,
+        footer
+      )}
+    </>
+    
   )
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer(){
+    return (
+        <div className="attribution">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+            Coded by <a href="https://cervantesdeveloper.com/" target="_blank">Cervantes Developer</a>.
+        </div>
+    )
+}
+
+export default Footer;
